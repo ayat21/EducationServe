e37cbe2a1cdd4be6a68235267cb9c3132f4be033
ayat hosam <ayathosam20@gmail.com>
2021-03-30 22:31:59 +0200

    fadfafaf

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError,
+  NavigationCancel } from '@angular/router';
+import { NgwWowService } from 'ngx-wow';
+import { AuthService } from '../user/auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  loading=true;
+  
+  constructor(private authService: AuthService,
+    private router: Router,  private wowService: NgwWowService) {
+    this.wowService.init();
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+   }
+   checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+
+      this.loading = true;
+    }
+
+    if (routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationCancel ||
+        routerEvent instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
+   get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
